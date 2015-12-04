97903649c48ab2d99b5e0a6a7be05d26202f0a05
Mukund Salia <mukund.salia@oracle.com>
2015-12-04 10:44:31 -0500

    Initializing with React

---
+++ src/Initializer.jsx
@@ -0,0 +1,50 @@
+/*
+ * Application Initializer
+ * @flow
+ */
+
+var App = require('App');
+var AppActions = require('AppActions');
+var AppConstants = require('AppConstants');
+var AppDispatcher = require('AppDispatcher');
+var ExampleStore = require('ExampleStore');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var {ActionTypes} = AppConstants;
+
+class Initializer {
+
+    constructor() {
+        this.application = <App />;
+        this.stores = [
+            ExampleStore,
+        ];
+    }
+
+    init(appRoot): void {
+        // Perform store initialization
+        var dispatchToken = AppDispatcher.register(action => {
+            if (action.type === ActionTypes.INIT_LOAD) {
+                AppDispatcher.waitFor(
+                    this.stores.map(store => store.getDispatchToken())
+                );
+
+                // Render the application
+                ReactDOM.render(this.application, appRoot);
+                AppDispatcher.unregister(dispatchToken);
+            }
+            return;
+        });
+
+        // send the initializing action
+        AppActions.initLoad();
+    }
+
+}
+
+// Let's initialize the application
+var initializer = new Initializer();
+initializer.init(document.getElementById("app-root"));
+
+module.exports = Initializer;
